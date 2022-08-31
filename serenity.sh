aa0d6fa8343a9577e8ab220159e5834dd187b3a7
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2022-08-31 12:09:45 +0100

    easier to run on different machines

--- serenity.sh
+++ serenity.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-export PATH=/home/robbie/serenity/Toolchain/Local/i686/bin:$PATH
+SERENITY_HOME=~/serenity
+
+export PATH=$SERENITY_HOME/Toolchain/Local/i686/bin:$PATH
 rm -rf build
-meson -Dfreestanding=true --cross-file ~/serenity/Build/i686/meson-cross-file.txt build
+meson -Dfreestanding=true --cross-file $SERENITY_HOME/Build/i686/meson-cross-file.txt build
 ninja -C build
-export DESTDIR=~/serenity/Build/i686/Root
+export DESTDIR=$SERENITY_HOME/Build/i686/Root
 meson install -C build
